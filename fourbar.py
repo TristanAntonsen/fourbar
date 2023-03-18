69f41ba71286e1fc9df568dfde1b8a1f000f8ad9
TristanAntonsen <tristan.antonsen@gmail.com>
2023-03-18 14:43:27 -0500

    starting Link and Linkage classes

--- fourbar.py
+++ fourbar.py
@@ -6,6 +6,18 @@ class Joint():
         self.x = x
         self.y = y
 
+class Link():
+    def __init__(self, j1, j2, length):
+        ### Assumes CCW order with origin as first link
+        self.j1 = j1
+        self.j2 = j2
+        self.length = length
+
+class Linkage():
+    def __init__(self, joints, links):
+        self.joints = joints
+        self.links = links
+
 ### Link lengths
 
 l0 = 7
@@ -20,7 +32,7 @@ jc = Joint(l0, 0)
 
 ### Drive angle
 
-theta2 = 100
+theta2 = 2 * np.pi / 2
 
 ### Calculating intermediate angles
 # Derivation of angles:
@@ -28,20 +40,24 @@ theta2 = 100
 
 a_c = np.sqrt(l0**2 + l1**2 + 2 * l0 * l1 * np.cos(theta2))
 beta = np.arccos((l0 + a_c**2 - l1**2) / (2 * l0 * a_c))
-psi = np.arccos((l2 ** 2 + a_c ** 2 - l1 ** 2) / (2 * l2 * a_c))
+psi = np.arccos((l2 ** 2 + a_c ** 2 - l3 ** 2) / (2 * l2 * a_c))
 lmda = np.arccos((l3 ** 2 + a_c ** 2 - l2 ** 2) / (2 * l3 * a_c))
 
 ### Calculating main angles
 
-if theta2 <= 180:
-    theta3 = psi - beta
-    theta4 = 180 - lmda - beta
-elif theta2 > 180:
+theta3 = psi - beta
+theta4 = np.pi - lmda - beta
+
+if theta2 > np.pi:
     theta3 = psi + beta
-    theta4 = 180 - lmda + beta
+    theta4 = np.pi - lmda + beta
 
 ### Remaining joint positions
 
-ja = Joint(l1 * np.cos(theta2), 0)
+ja = Joint(l1 * np.cos(theta2), l1 * np.cos(theta2))
 jb = Joint(jc.x + l3 * np.cos(theta4), l3 * np.sin(theta4))
+joints = [ja, jb, jo, jc]
+for j in joints:
+    print(j.x, j.y)
 
+display(1080, 1080, joints, save=False, show=True)
