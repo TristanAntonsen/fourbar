6c69df78b37f5c74337dd75248299fd16e68ddc7
TristanAntonsen <tristan.antonsen@gmail.com>
2023-03-18 22:44:20 -0500

    adding length validation, still stretching

--- fourbar.py
+++ fourbar.py
@@ -3,19 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+
 class Joint():
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+
 class Link():
-    def __init__(self, js, je, length):
+    def __init__(self, js, je):
         self.js = js
         self.je = je
-        self.length = length
+        self.length = self.calc_length()
+
+    def calc_length(self):
+        return np.sqrt((self.je.x-self.js.x) ** 2 + (self.je.y-self.js.y) ** 2)
+
 
 class Linkage():
     def __init__(self, jo, jc, l0, l1, l2, l3):
+        self.theta = 45
         self.jo = jo
         self.ja = Joint(None, None)
         self.jc = jc
@@ -25,20 +32,39 @@ class Linkage():
         self.l2 = l2
         self.l3 = l3
 
+    def validate_lengths(self):
+        link0 = Link(self.jo, self.jc)
+        link1 = Link(self.jo, self.ja)
+        link2 = Link(self.ja, self.jb)
+        link3 = Link(self.jb, self.jc)
+        d0 = round((link0.length - self.l0) / self.l0, 3)
+        d1 = round((link1.length - self.l1) / self.l1, 3)
+        d2 = round((link2.length - self.l2) / self.l2, 3)
+        d3 = round((link3.length - self.l3) / self.l3, 3)
+
+        print(d0, d1, d2, d3)
+
+        # if l3 + l2 > l1 + l0:
+        #     print("INVALID")
+
     def calculate_positions(self, theta2):
 
         if theta2 > np.pi * 2:
             theta2 = theta2 - np.pi * 2
-        ### Calculating intermediate angles using law of cosines
+        # Calculating intermediate angles using law of cosines
         # Derivation of angles:
         # https://www.youtube.com/watch?v=4O-XPJ7flLU
 
-        a_c = np.sqrt(self.l0 ** 2 + self.l1 ** 2 - 2 * self.l0 * self.l1 * np.cos(theta2))
-        beta = np.arccos((self.l0 ** 2 + a_c ** 2 - self.l1 ** 2) / (2 * self.l0 * a_c))
-        psi = np.arccos((self.l2 ** 2 + a_c ** 2 - self.l3 ** 2) / (2 * self.l2 * a_c))
-        lmda = np.arccos((self.l3 ** 2 + a_c ** 2 - self.l2 ** 2) / (2 * self.l3 * a_c))
+        a_c = np.sqrt(self.l0 ** 2 + self.l1 ** 2 - 2 *
+                      self.l0 * self.l1 * np.cos(theta2))
+        beta = np.arccos((self.l0 ** 2 + a_c ** 2 -
+                         self.l1 ** 2) / (2 * self.l0 * a_c))
+        psi = np.arccos((self.l2 ** 2 + a_c ** 2 -
+                        self.l3 ** 2) / (2 * self.l2 * a_c))
+        lmda = np.arccos((self.l3 ** 2 + a_c ** 2 -
+                         self.l2 ** 2) / (2 * self.l3 * a_c))
 
-        ### Calculating main angles
+        # Calculating main angles
 
         theta3 = psi - beta
         # theta4 = np.pi - lmda - beta
@@ -47,37 +73,52 @@ class Linkage():
             theta3 = psi + beta
             theta4 = np.pi - lmda + beta
 
-        ### Joints
+        # Joints
         self.ja.x = self.l1 * np.cos(theta2)
         self.ja.y = self.l1 * np.sin(theta2)
 
         self.jb.x = self.jo.x + self.ja.x + self.l2 * np.cos(theta3)
         self.jb.y = self.jo.y + self.ja.y + self.l2 * np.sin(theta3)
 
+        self.validate_lengths()
 
-def animate_linkage(linkage, interval):
+        return self.jo, self.ja, self.jc, self.jb
 
-    fig = plt.figure()
-    ax = plt.axes(xlim=(-5, 10), ylim=(-5, 10))
-    line, = ax.plot([], [], lw=3)
+    def animate(self):
 
-    frames = 360
+        co1 = plt.Circle((self.jo.x, self.jo.y), self.l1,
+                              color='r', fill=False, linewidth=1, linestyle="dashed")
+        cc1 = plt.Circle((self.jc.x, self.jc.y), self.l3,
+                              color='r', fill=False, linewidth=1, linestyle="dashed")
 
-    def init():
-        line.set_data([], [])
-        return line,
-    def animate(i):
-        theta = i * np.pi / 180
-        linkage.calculate_positions(theta)
-        x = [linkage.jo.x, linkage.ja.x, linkage.jb.x, linkage.jc.x, linkage.jo.x]
-        y = [linkage.jo.y, linkage.ja.y, linkage.jb.y, linkage.jc.y, linkage.jo.y]
-        line.set_data(x, y)
-        return line,
+        fig = plt.figure(figsize=(5, 5))
+        ax = plt.axes(xlim=(-5, 10), ylim=(-5, 10))
 
-    anim = FuncAnimation(fig, animate, init_func=init,
-                                frames=frames, interval=interval, blit=True)
+        ax.add_patch(co1)
+        ax.add_patch(cc1)
 
-    plt.show()
+        line, = ax.plot([], [], lw=3)
+
+        frames = 360
+
+        def init():
+            line.set_data([], [])
+            return line,
+
+        def animate(i):
+            theta = i * np.pi / 180
+            self.calculate_positions(theta)
+            x = [self.jo.x, self.ja.x, self.jb.x, self.jc.x, self.jo.x]
+            y = [self.jo.y, self.ja.y, self.jb.y, self.jc.y, self.jo.y]
+            line.set_data(x, y)
+            return line,
+
+        interval = 10
+        anim = FuncAnimation(fig, animate, init_func=init,
+                             frames=frames, interval=interval, blit=True)
+
+        plt.show()
+
 
 if __name__ == "__main__":
 
@@ -85,8 +126,9 @@ if __name__ == "__main__":
     l1 = 3
     l2 = 7.5
     l3 = 4.5
-    origin = Joint(1, 1)
-    anchor = Joint(l0, 0)
-    fourbar = Linkage(origin, anchor, l0, l1, l2, l3)
+    jo = Joint(1, 1)
+    jc = Joint(l0, 0)
 
-    animate_linkage(fourbar, 10)
+    fourbar = Linkage(jo, jc, l0, l1, l2, l3)
+
+    fourbar.animate()
